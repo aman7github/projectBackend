c42cea8e1f37936b1653a237fc838b07c13d90b1
aman7github <amanmaurya9044@gmail.com>
2023-04-02 18:43:44 +0530

    edit order

--- routes/Order.route.js
+++ routes/Order.route.js
@@ -9,9 +9,9 @@ const {OrderModel}= require("../model/AllDeals.model")
 OrderRoute.post("/add",async(req,res)=>{
 
     try{
-       const newcompany = new OrderModel(req.body)
-       await newcompany.save()
-       //await OrderModel.insertMany(req.body.restaurants)
+    //    const newcompany = new OrderModel(req.body)
+    //    await newcompany.save()
+       await OrderModel.insertMany(req.body.user)
        res.status(200).send({"msg":"new Order added"})
 
 
